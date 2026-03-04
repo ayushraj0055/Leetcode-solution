be9fb7bdea6b654fc3c0bdc42c74eb3dd7bcec28
ayushraj0055 <ayusraj0055@gmail.com>
2026-03-04 09:26:02 +0530

    Time: 4 ms (73.45%) | Memory: 45.1 MB (68.23%) - LeetSync

---
+++ week2/496-next-greater-element-i/next-greater-element-i.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+
+        // Find next greater element for nums2
+        for(int num : nums2) {
+
+            while(!stack.isEmpty() && num > stack.peek()) {
+                map.put(stack.pop(), num);
+            }
+
+            stack.push(num);
+        }
+
+        // Remaining elements have no next greater
+        while(!stack.isEmpty()) {
+            map.put(stack.pop(), -1);
+        }
+
+        int[] result = new int[nums1.length];
+
+        for(int i = 0; i < nums1.length; i++) {
+            result[i] = map.get(nums1[i]);
+        }
+
+        return result;
+    }
+}
